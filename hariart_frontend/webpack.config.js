0575a634151144822bfd399da37b4debb87c8733
mskab <mskab2000@gmail.com>
2022-04-20 12:38:18 +0300

    Fix display picture of background

--- hariart_frontend/webpack.config.js
+++ hariart_frontend/webpack.config.js
@@ -12,15 +12,8 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|jpg)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: false,
-            },
-          },
-        ],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
       {
         resolve: {
