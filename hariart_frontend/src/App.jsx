b6a54d527ab41ad3b6d61c0d539f6baaf8b39250
mskab <mskab2000@gmail.com>
2022-04-21 00:08:19 +0300

    Add about us page and footer

--- hariart_frontend/src/App.jsx
+++ hariart_frontend/src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
-import Navbar from './components/navbar/Navbar';
-import "./App.css"
+import Navbar from "./components/navbar/Navbar";
+import About from "./components/about/About";
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/authors" element={<About />} />
       </Routes>
     </Router>
   );
