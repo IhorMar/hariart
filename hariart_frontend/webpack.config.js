e49af86376fecf68681ab4f1dc527f368c25097b
mskab <mskab2000@gmail.com>
2022-04-22 21:34:45 +0300

    Add font styles

--- hariart_frontend/webpack.config.js
+++ hariart_frontend/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ttf|otf)$/i,
         type: "asset/resource",
       },
       {
