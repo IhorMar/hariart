9d4a0f607c4ca2c9f50bc7c7731dcae7a1059416
mskab <mskab2000@gmail.com>
2022-04-22 15:07:13 +0300

    Add Contacts page and validation for form on it

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('hariart.urls')),
     path('', include('hariart_frontend.urls')),
-    re_path(r'^(?:.*)/?$', include('hariart_frontend.urls')),
+    re_path(r'^(?:.*)/?', include('hariart_frontend.urls')),
 ]
--- hariart_frontend/src/App.jsx
+++ hariart_frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import About from "./components/about/About";
+import Contacts from "./components/contacts/Contacts";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/authors" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
       </Routes>
     </Router>
   );
