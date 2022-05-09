f3867197962772a3928fb5a89a2d421e7cfdf465
mskab <mskab2000@gmail.com>
2022-05-09 17:15:58 +0300

    Add painting details page, refactoring URLs and paintings group page

---
+++ hariart_frontend/src/components/painting_details/Painting_details.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { getLastSegment } from "../../utils/URL";
+import PaintingsHandler from "../../services/handlers/Paintings";
+import Footer from "../footer/Footer";
+import "./Painting_details.css";
+import Picture from "../../picture/Picture";
+
+export default function PaintingDetails({ category }) {
+  const location = useLocation();
+  const [picture, setPicture] = useState();
+
+  useEffect(() => {
+    PaintingsHandler.getPictureByParams(
+      { ref: getLastSegment(location) },
+      ({ results }) =>
+        PaintingsHandler.getSizeOfPictureByRef(results[0].ref, (res) =>
+          setPicture({
+            ...results[0],
+            size: `${res.results[0].height}x${res.results[0].width}`,
+          })
+        )
+    );
+  }, []);
+
+  return (
+    <>
+      {picture && (
+        <div className="painting-details">
+          <div className="painting-details__header">
+            <Picture />
+            <div className="header__info">
+              <div className="info">
+                <div className="title">{picture.name}</div>
+                <div className="size">{picture.size}</div>
+              </div>
+              <button className="button">Send request for the price</button>
+            </div>
+          </div>
+          <div className="description">{picture.description}</div>
+        </div>
+      )}
+      <Footer />
+    </>
+  );
+}
---
+++ hariart_frontend/src/services/handlers/Paintings.jsx
@@ -0,0 +1,30 @@
+import axios from "../Config";
+
+const PaintingsHandler = {
+  getPictureByParams(params, callbacks) {
+    return axios
+      .get("paintings/", {
+        params: { ...params },
+      })
+      .then(({data}) => {
+        callbacks(data)
+      })
+      .catch((error) => {
+        Promise.reject(error);
+      });
+  },
+  getSizeOfPictureByRef(ref, callbacks) {
+    return axios
+      .get("painting_sizes/", {
+        params: { ref: ref },
+      })
+      .then(({data}) => {
+        callbacks(data)
+      })
+      .catch((error) => {
+        Promise.reject(error);
+      });
+  },
+};
+
+export default PaintingsHandler;
