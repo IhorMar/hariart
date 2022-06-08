bd52867cb6efccd4b8f031a370af0f6b014c3dde
mskab <mskab2000@gmail.com>
2022-06-08 23:11:13 +0300

    Add configuration for deployment

--- config/settings.py
+++ config/settings.py
@@ -109,10 +109,6 @@ USE_TZ = True
 
 STATIC_URL = 'hariart_frontend/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "hariart_frontend/static"),
-#     '/var/www/static/',
-# ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
