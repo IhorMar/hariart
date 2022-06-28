ac4c8474fa1cd138d2bacf61bfc36dd7f96ce384
mskab <mskab2000@gmail.com>
2022-06-28 20:23:49 +0300

    update urls

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path('api/', include('hariart.urls')),
+    path(r'admin', admin.site.urls),
+    re_path(r'api', include('hariart.urls')),
     path('', include('hariart_frontend.urls')),
     re_path(r'^(?:.*)/?', include('hariart_frontend.urls')),
 ]
