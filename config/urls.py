71c2014dc1504862cb390b2f016274437bcca17c
Valentyn Slivko <metallistvalon@gmail.com>
2022-07-09 22:42:05 +0300

    Resolved reverse bug, merged with bug branch

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('hariart.urls')),
-    path('', include('hariart_frontend.urls')),
-    re_path(r'^(?:.*)/?', include('hariart_frontend.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("hariart.urls")),
+    path("", include("hariart_frontend.urls")),
+    re_path(r"^(?:.*)/?", include("hariart_frontend.urls")),
 ]
--- hariart/views.py
+++ hariart/views.py
@@ -1,22 +1,36 @@
 from django.http import HttpResponse
-from django.core.mail import send_mail, BadHeaderError
+from django.core.mail import BadHeaderError
 from django.http import HttpResponse
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
-from .models import Painting, PaintingSize, PaintingOrder, Order, Contact, COUNTRY_CHOICES
-from .serializers import PaintingSerializer, PaintingSizeSerializer, OrderSerializer, PaintingOrderSerializer, ContactSerializer
+from .models import (
+    Painting,
+    PaintingSize,
+    PaintingOrder,
+    Order,
+    Contact,
+    COUNTRY_CHOICES,
+)
+from .serializers import (
+    PaintingSerializer,
+    PaintingSizeSerializer,
+    OrderSerializer,
+    PaintingOrderSerializer,
+    ContactSerializer,
+)
 from .tasks import send_email_on_new_order, send_email_contact_us
+
 
 class PaintingViewSet(viewsets.ModelViewSet):
     serializer_class = PaintingSerializer
     queryset = Painting.objects.all()
-    filterset_fields = ['category', 'ref']
+    filterset_fields = ["category", "ref"]
 
 
 class PaintingSizeViewSet(viewsets.ModelViewSet):
     serializer_class = PaintingSizeSerializer
     queryset = PaintingSize.objects.all()
-    filterset_fields = ['painting']
+    filterset_fields = ["painting"]
 
 
 class ConactViewSet(viewsets.ModelViewSet):
@@ -33,37 +47,46 @@ class OrderViewSet(viewsets.ModelViewSet):
             post_data = request.data
 
             orderLatestId = self.queryset.create(
-                customer_name=post_data['name'],
-                customer_surname=post_data['surname'],
-                customer_email=post_data['email'],
-                customer_phone=post_data['phone'],
-                country=post_data['country']['name']
+                customer_name=post_data["name"],
+                customer_surname=post_data["surname"],
+                customer_email=post_data["email"],
+                customer_phone=post_data["phone"],
+                country=post_data["country"]["name"],
             )
 
-            paintings = list(map(lambda painting: PaintingOrder(
-                painting=Painting.objects.get(pk=painting['ref']),
-                order=orderLatestId,
-                quantity=painting['amount']
-            ), post_data['paintings']))
+            paintings = list(
+                map(
+                    lambda painting: PaintingOrder(
+                        painting=Painting.objects.get(pk=painting["ref"]),
+                        order=orderLatestId,
+                        quantity=painting["amount"],
+                    ),
+                    post_data["paintings"],
+                )
+            )
 
             PaintingOrder.objects.bulk_create(paintings)
 
-            contact_emails = Contact.objects.filter(country=post_data['country']['id']).values('email')
+            contact_emails = Contact.objects.filter(
+                country=post_data["country"]["id"]
+            ).values("email")
 
             subject = f'{post_data["name"]} {post_data["surname"]} made an order'
             message = f'Customer {post_data["name"]} {post_data["surname"]} wanted to order paintings.\n{displayPaintingsForMessage(post_data["paintings"])}\n\nClient email: {post_data["email"]}\n\nClient phone: {post_data["phone"]}'
-            sender = 'mskabwork@gmail.com'
-            recipients = list(map(lambda contact_email: contact_email['email'], contact_emails))
+            sender = "mskabwork@gmail.com"
+            recipients = list(
+                map(lambda contact_email: contact_email["email"], contact_emails)
+            )
 
             try:
                 send_email_on_new_order.delay(subject, message, sender, recipients)
             except BadHeaderError:
-                return HttpResponse('Invalid header in send email found')
+                return HttpResponse("Invalid header in send email found")
 
         except:
-            return HttpResponse('Invalid header found')
+            return HttpResponse("Invalid header found")
 
-        return HttpResponse('Success...Your email has been sent')
+        return HttpResponse("Success...Your email has been sent")
 
 
 class PaintingOrderViewSet(viewsets.ModelViewSet):
@@ -71,38 +94,57 @@ class PaintingOrderViewSet(viewsets.ModelViewSet):
     queryset = PaintingOrder.objects.all()
 
 
-@api_view(['POST'])
+@api_view(["POST"])
 def contact_us(request):
     try:
         post_data = request.data
 
-        contact_emails = Contact.objects.filter(country__in=checkCountryByLanguage(post_data["language"])).values('email')
+        contact_emails = Contact.objects.filter(
+            country__in=checkCountryByLanguage(post_data["language"])
+        ).values("email")
 
         subject = f'Message from {post_data["name"]}'
         message = f'Customer {post_data["name"]} sended you a message.\n\n{post_data["message"]}\n\nClient email: {post_data["email"]}\n\nClient phone: {post_data["phone"]}'
-        sender = 'mskabwork@gmail.com'
-        recipients = list(map(lambda contact_email: contact_email['email'], contact_emails))
+        sender = "mskabwork@gmail.com"
+        recipients = list(
+            map(lambda contact_email: contact_email["email"], contact_emails)
+        )
 
         try:
             send_email_contact_us.delay(subject, message, sender, recipients)
         except BadHeaderError:
-            return HttpResponse('Invalid header in send email found')
+            return HttpResponse("Invalid header in send email found")
 
     except BadHeaderError:
-        return HttpResponse('Invalid header found')
+        return HttpResponse("Invalid header found")
 
-    return HttpResponse('Success...Your email has been sent')
+    return HttpResponse("Success...Your email has been sent")
+
 
 def displayPaintingsForMessage(paintingsDetail):
-    return "\n".join(list(map(lambda painting: f'{painting["name"]} x {painting["amount"]}', paintingsDetail)))
+    return "\n".join(
+        list(
+            map(
+                lambda painting: f'{painting["name"]} x {painting["amount"]}',
+                paintingsDetail,
+            )
+        )
+    )
+
 
 def checkCountryByLanguage(language):
-    if (language == "lt"):
+    if language == "lt":
         countriesRelateToLang = ["LT"]
-    elif (language == "ru"):
+    elif language == "ru":
         countriesRelateToLang = ["RU", "UA"]
     else:
         countriesRelateToLang = ["NW"]
- 
-    return list(map(lambda country: country[0], filter(lambda country: country[0] in countriesRelateToLang, COUNTRY_CHOICES)))
 
+    return list(
+        map(
+            lambda country: country[0],
+            filter(
+                lambda country: country[0] in countriesRelateToLang, COUNTRY_CHOICES
+            ),
+        )
+    )
